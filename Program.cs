7aa1b3d892233890a14dcf831efe24a26ec2b194
vp coder <veenaparihar2002@gmail.com>
2024-11-21 23:43:11 +0530

    run migration and connect db

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using TentBookingManagement.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var provider = builder.Services.BuildServiceProvider();
+var config = provider.GetRequiredService<IConfiguration>();
+builder.Services.AddDbContext<TentInventoryDbContext    >(item => item.UseSqlServer(config.GetConnectionString("dbset")));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<TentInventoryDbContext>();
+    context.Database.Migrate(); // This line applies migrations and creates the database
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
